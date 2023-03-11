c077b216ef797da94574d776e0021f9054dfff77
magic-zhu <18817313320@163.com>
2023-03-11 21:11:31 +0800

    input select

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,4 +1,6 @@
 import Button from "./components/Button/Button.vue"
 import MIcon from "./components/MIcon/MIcon.vue"
 import AttributeTitle from "./components/AttributeTitle/AttributeTitle.vue"
-export { Button, MIcon, AttributeTitle }
+import MInput from "./components/MInput/MInput.vue"
+import MSelect from "./components/MSelect/MSelect.vue"
+export { Button, MIcon, AttributeTitle, MInput, MSelect }
